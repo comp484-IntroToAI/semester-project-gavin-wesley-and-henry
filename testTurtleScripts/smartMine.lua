a8b447f0ffa1cd0bd7062ba80c0c7adfb09e3885
Henry Heyden <henryden22@gmail.com>
2024-10-29 21:06:07 -0500

    :)

--- testTurtleScripts/smartMine.lua
+++ testTurtleScripts/smartMine.lua
@@ -20,6 +20,14 @@ end
 
 searchForDiamonds()
 
+local function mineVein()
+    -- TODO by Wesley
+    -- You are face to face with the block that you would like to mine.
+    -- Mine a 5x5 cube around it
+    -- Don't worry about inventory management, and just worry about making it work on Diamonds for now
+    -- make sure to use functions from smartActions (smac)
+end
+
 
 local function stickToSurface()
     -- TODO: make a function that, when called, has the turtle go down until it is resting on a surface block (check for leaves?)
